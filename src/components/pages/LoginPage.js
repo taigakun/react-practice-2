84c6ca492c55728ae85d5dc34ce62c1f4fe04a47
Taiga Matsumoto <matsumoto26761192@gmail.com>
2018-02-11 20:59:28 +1000

    going well

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
+class LoginPage extends React.Component{
 
-const LoginPage = () => (
-  <div>
-    <h1>Login page</h1>
+  submit = (data) => {
+    console.log(data);
+  };
 
-  </div>
-);
+  render(){
+    return(
+      <div>
+        <h1>Login page</h1>
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
