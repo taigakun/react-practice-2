391f3fe2166d183219a6a58af9f929d53bd0489c
Taiga Matsumoto <matsumoto26761192@gmail.com>
2018-02-13 23:42:52 +1000

    update txt file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from "./rootReducer";
 
-ReactDOM.render(<BrowserRouter>
-  <App/>
-</BrowserRouter>, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
